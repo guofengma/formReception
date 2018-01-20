859595a4ef03a9e19f4ab314818b01d750a65663
zhaoqing <walkingzq@163.com>
2018-01-20 15:14:35 +0800

    微调

--- src/main/java/web/utils/TimeUtil.java
+++ src/main/java/web/utils/TimeUtil.java
@@ -3,8 +3,10 @@ package web.utils;
 import web.entity.Records;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by qaa on 2017/10/30.
@@ -13,13 +15,21 @@ import java.util.Date;
 public class TimeUtil {
     private static Calendar calendar = Calendar.getInstance();
 
+    static class OOMObject{}
+
     public static void main(String args[]){
+
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        while (true){
+            list.add(new OOMObject());
+        }
+
 //        System.out.println(calendar.getTime());
 //        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
 //        System.out.println(simpleDateFormat.format(calendar.getTime()));
 //        System.out.println(TimeUtil.nowTime("YYYY-MM-dd HH:mm:ss"));
-        Records records = new Records();
-        System.out.println(records.getSubmitTime());
+//        Records records = new Records();
+//        System.out.println(records.getSubmitTime());
     }
 
     public static String nowTime(String dateFormat){
